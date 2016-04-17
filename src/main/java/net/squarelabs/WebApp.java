ed6b26c413eb6ec2da533ac106d882a38c2af77b
Brent Gardner <bgardner@squarelabs.net>
2016-04-16 21:56:18 -0600

    Able to load categories

--- src/main/java/net/squarelabs/WebApp.java
+++ src/main/java/net/squarelabs/WebApp.java
@@ -1,14 +1,33 @@
 package net.squarelabs;
+
+import org.apache.commons.io.IOUtils;
+
+import java.io.InputStream;
+import java.net.URL;
+import java.net.URLConnection;
+
 import static spark.Spark.*;
 
-/**
- * Created by rachelkoldenhoven on 4/16/16.
- */
 public class WebApp {
-    public static void main(String[] args) {
-        staticFileLocation("/public"); // Static files
-        get("/hello", (req, res) -> "Hello World");
+  private static final String BASE_URL = "https://api.meetup.com/";
+  private static final String GET_CATEGORIES = BASE_URL + "2/categories";
 
-        System.out.println("Web app started! Please browse to http://localhost:4567/");
-    }
+  public static void main(String[] args) {
+    staticFileLocation("/public"); // Static files
+
+    get("/hello", (req, res) -> "Hello World");
+    get("/categories", (req, res) -> {
+      String key = req.queryParams("key");
+      URL url = new URL(GET_CATEGORIES + "?key=" + key);
+      URLConnection con = url.openConnection();
+      try (InputStream in = con.getInputStream()) {
+        String encoding = con.getContentEncoding();
+        encoding = encoding == null ? "UTF-8" : encoding;
+        String body = IOUtils.toString(in, encoding);
+        return body;
+      }
+    });
+
+    System.out.println("Web app started! Please browse to http://localhost:4567/");
+  }
 }
--- src/main/resources/public/js/controllers.js
+++ src/main/resources/public/js/controllers.js
@@ -1,5 +1,14 @@
-app.controller('MainController', ['$scope', function($scope) {
+"use strict";
+
+app.controller('MainController', ['$scope', '$http', function ($scope, $http) {
+
     $scope.view = {
         message: 'Hello world!!!'
+    };
+
+    $scope.apiKeySubmit = function () {
+        const url = "/categories?key=" + $scope.view.apiKey;
+        $http.get(url)
+            .then((data) => $scope.view.categories = data.data.results);
     };
 }]);
