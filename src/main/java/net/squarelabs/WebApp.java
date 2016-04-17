b35bb6984008bd1174649bbc1b8de927e65e9c4d
Brent Gardner <bgardner@squarelabs.net>
2016-04-17 10:15:30 -0600

    Can retrieve (first 200) groups

--- src/main/java/net/squarelabs/WebApp.java
+++ src/main/java/net/squarelabs/WebApp.java
@@ -10,15 +10,26 @@ import static spark.Spark.staticFileLocation;
 public class WebApp {
   private static final String BASE_URL = "https://api.meetup.com/";
   private static final String GET_CATEGORIES = BASE_URL + "2/categories";
+  private static final String GET_GROUPS = BASE_URL + "2/groups";
 
   public static void main(String[] args) {
     staticFileLocation("/public"); // Static files
 
-    get("/hello", (req, res) -> "Hello World");
     get("/categories", (req, res) -> {
       String key = req.queryParams("key");
       res.type("application/json");
       return IOUtils.toString(new URL(GET_CATEGORIES + "?key=" + key));
+    });
+
+    get("/groups", (req, res) -> {
+      String url = String.format("%s?key=%s&category_id=%s&zip=%s&radius=%s",
+          GET_GROUPS,
+          req.queryParams("key"),
+          req.queryParams("category_id"),
+          req.queryParams("zip"),
+          req.queryParams("radius")
+      );
+      return IOUtils.toString(new URL(url));
     });
 
     System.out.println("Web app started! Please browse to http://localhost:4567/");
--- src/main/resources/public/js/controllers.js
+++ src/main/resources/public/js/controllers.js
@@ -3,12 +3,30 @@
 app.controller('MainController', ['$scope', '$http', function ($scope, $http) {
 
     $scope.view = {
-        message: 'Hello world!!!'
+        radius: 50
     };
 
     $scope.apiKeySubmit = function () {
         const url = "/categories?key=" + $scope.view.apiKey;
         $http.get(url)
-            .then((data) => $scope.view.categories = data.data.results);
+            .then((data) =>
+                $scope.view.categories = data.data.results
+            );
+    };
+
+    $scope.zipCodeSubmit = function () {
+        const categoryId = $scope.view.categories
+            .filter((cat) => cat.selected)
+            .map((cat) => cat.id)[0];
+        const url = "/groups"
+                + "?key=" + $scope.view.apiKey
+                + "&zip=" + $scope.view.zip
+                + "&radius=" + $scope.view.radius
+                + "&category_id=" + categoryId
+            ;
+        $http.get(url)
+            .then((data) =>
+                $scope.view.groups = data.data.results
+            );
     };
 }]);
