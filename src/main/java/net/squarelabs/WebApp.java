baba0d4c1f16f82470c2779f9c4fa96f761930c9
RachelKoldenhoven <rachel@koldenfrozen.com>
2016-04-16 20:04:35 -0600

    Created class for web app.

---
+++ src/main/java/net/squarelabs/WebApp.java
@@ -0,0 +1,10 @@
+package net.squarelabs;
+
+/**
+ * Created by rachelkoldenhoven on 4/16/16.
+ */
+public class WebApp {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
