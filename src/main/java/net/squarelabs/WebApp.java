428e45f800ac63ff3b1171ceb1b6964c6c014cf0
RachelKoldenhoven <rachel@koldenfrozen.com>
2016-04-16 20:18:57 -0600

    Set up static route to serve web files.

--- src/main/java/net/squarelabs/WebApp.java
+++ src/main/java/net/squarelabs/WebApp.java
@@ -7,6 +7,7 @@ import static spark.Spark.*;
 public class WebApp {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        staticFileLocation("/public"); // Static files
         get("/hello", (req, res) -> "Hello World");
     }
 }
